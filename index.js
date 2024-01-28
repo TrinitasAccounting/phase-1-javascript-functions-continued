b982c01d0a265e312f852c19655f8254101d2e42
Clay Mangum <clay@trinitasaccounting.com>
2024-01-28 11:08:26 -0600

    complete

--- index.js
+++ index.js
@@ -1,1 +1,22 @@
 // code your solution here
+
+
+// const saturdayFun = (arg = 'roller-skate') => {
+//     return arg;
+// }
+
+
+function saturdayFun(arg = 'roller-skate') {
+    return `This Saturday, I want to ${arg}!`;
+}
+
+
+const mondayWork = (arg = 'go to the office') => {
+    return `This Monday, I will ${arg}.`;
+}
+
+const wrapAdjective = (str = '*') => {
+    return function (param = 'special') {
+        return `You are ${str}${param}${str}!`
+    };
+}
